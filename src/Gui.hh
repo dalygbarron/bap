80ca4ec68a471065c0b1ea2481342dfa5548739f
daly barron <dalygbarron@gmail.com>
2020-10-18 12:02:34 +0100

    whacking off

--- src/Gui.hh
+++ src/Gui.hh
@@ -4,23 +4,44 @@
 #include <vector>
 
 /**
- * An element of the gui.
- */
-class GuiElement {
-    public:
-        std::vector<GuiElement> children;
-};
-
-/**
  * Stores gui shiet.
  */
 class Gui {
     public:
+        /**
+         * Creates the gui thingy.
+         */
         Gui();
+
+        /**
+         * Places a gui thingy on the screen in some place, then returns if
+         * there is a user interaction result.
+         * @param bounds is the bounds within which to fit the gui thingy.
+         * @param item   is basically the gui item to fit.
+         * @return a result from gui items or a value less than 0 if there was
+         *         none.
+         */
+        int place(SDL_Rect bounds, SDL_Rect item);
+
+        /**
+         * Creates a panel that is containing other stuff.
+         * @param child is as many children as you want the panel to have.
+         */
+        SDL_Rect panel(SDL_Rect child...);
+
+        SDL_Rect button(SDL_Rect child);
 
 
     private:
-        std::vector<GuiElement> items
+        SDL_Rect bounds;
+        int result;
+
+        /**
+         * Tries to set a gui interaction result.
+         * @param result is the result to set but if one is already set nothing
+         *               happens.
+         */
+        void setResult(int result);
 };
 
 #endif
